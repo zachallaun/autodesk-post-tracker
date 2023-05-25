715bc9eb41cafd392674f4d2b1f55d65c61dd63f
Zach Allaun <zach.allaun@gmail.com>
2023-05-25 14:02:18 -0400

    wip: downloader

--- download.exs
+++ download.exs
@@ -57,4 +57,49 @@ defmodule Download do
   end
 end
 
-Download.changes() |> List.first() |> Download.post() |> IO.inspect()
+defmodule GitOps do
+  def initial_commit do
+    git(["rev-list", "--max-parents=0", "main"])
+  end
+
+  def checkout_and_maybe_init(name) do
+    with {:ok, init} <- initial_commit(),
+         {:ok, _} <- git(["checkout", "-b", name, init]) do
+      :ok
+    else
+      # branch already exists
+      {:error, 128, _} -> checkout(name)
+    end
+  end
+
+  def last_message do
+    git(["log", "--pretty=format:%s", "-n1"])
+  end
+
+  def checkout(name) do
+    git(["checkout", name])
+  end
+
+  def stash do
+    git(["stash"])
+  end
+
+  def stash_pop do
+    git(["stash", "pop"])
+  end
+
+  def git(command) do
+    case System.cmd("git", command) do
+      {out, 0} -> {:ok, String.trim(out)}
+      {err, code} -> {:error, code, err}
+    end
+  end
+end
+
+with {:ok, _} <- GitOps.stash(),
+     resp <- GitOps.checkout_and_maybe_init("testing"),
+     {:ok, _} <- GitOps.checkout("main"),
+     {:ok, _} <- GitOps.stash_pop() do
+  resp
+end
+|> IO.inspect()
